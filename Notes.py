dd59b32d45f97c9eb81eb85645df2907fcc15a68
BalchenkoSergiy <sergiybalchenko@gmail.com>
2024-01-25 10:48:54 -0600

    moved temporary files to DRAFT

---
+++ Notes.py
@@ -0,0 +1,54 @@
+'Notes'
+    Shared folder: engineeredcode.sharepoint.com
+    
+    Intel Core i7 10700F @ 2.90GHz	33 °C
+    Comet Lake 14nm Technology
+
+'Tamplate for video'
+    Hello everyone and welcome to Power Pages Tips.
+
+    My name is Nicholas Hayduk, and this is Tip #199: Configuring Multistep Forms in the Studio
+    
+    While the ability to add a form, previous known as basic forms, or entity forms, to a page has existed in the studio since the Power Apps Portals days, adding a multistep form, also known as advanced form or web form, has not been possible. That is, until recently, when Microsoft has added new functionality to allow users to create multistep forms directly in the Power Pages studio.
+    
+    When creating a multistep form, or editing an existing one, users are able to control features like whether to show the progress indicator, allowing multiple entries per person, what to do on submit, and CAPTCHA.
+    
+    Then, users are able to add steps to the form, including picking the table and form, and other settings like attachments. Similar to what was discussed in Tip #198 for single step forms, users can edit field settings like labels, descriptions and validation on the form itself.
+    
+    Once steps are added, users can reorder them. Users can also update settings related to the progress indicator, if applicable.
+    
+    Multistep forms in Power Pages offer a lot of functionality, and what is currently available in the studio just scratches the surface. You'll still need to go to the Portal Management app to do things like conditional steps, action buttons and more complex metadata. I expect Microsoft to continue to expand on the currently available features, reducing the need to go over to the Portal Management app more and more.
+    
+    Thanks for watching and I hope you found the Power Pages Tip number 199 on Configuring Multistep Forms in the Studio, useful.
+    
+    --------------------------------------------------
+    
+    [VIDEO SCHEDULING]
+    
+    Scheduled for: 2023-01-24
+    
+    --------------------------------------------------
+    
+    [YouTube]
+    
+    Title: 'Power Pages Tip #199 - Configuring Multistep Forms in the Studio - Engineered Code'
+    
+    Description: 'In this tip, I look at the functionality that allows you to configure multistep forms (aka advanced forms, or web forms) in the Power Pages studio.'
+    
+    Tags: 'power pages, power apps portals,power apps,power platform,dynamics 365,dynamics 365 portal,dynamics 365 portals,portal capabilities for dynamics 365'
+    
+    --------------------------------------------------
+    
+    [Facebook and Twitter]
+    
+    #PowerPages Tip 199 - Configuring Multistep Forms in the Studio #PowerAppsPortals #PowerApps #MSDyn365Portals #MSDyn365 https://youtu.be/7JPN9_KbPDY
+    
+    --------------------------------------------------
+    
+    [LinkedIn]
+    
+    Power Pages Tip #199 - Configuring Multistep Forms in the Studio - Engineered Code
+    
+    In this tip, I look at the functionality that allows you to configure multistep forms (aka advanced forms, or web forms) in the Power Pages studio.
+    
+    #PowerPages #PowerApps #MSDyn365Portals #MSDyn365
